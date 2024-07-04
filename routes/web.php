0163bc87c733ef24f48b749766e06a651ec0517a
Joaquin Renato Martinez Olivares <132187041+Jrenat0@users.noreply.github.com>
2024-07-04 00:32:00 -0400

    create login.blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',[HomeController::class,'index'])->name('home.index');
+
+
+
+
+Route::get('/usuarios/login',[UsuariosController::class,'login'])->name('usuarios.login');
